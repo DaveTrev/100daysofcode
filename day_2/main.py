4b76e9ecea817a5485e2835f803dc3ad7f271ec5
DaveTrev <trevaskisdavid@gmail.com>
2024-03-10 08:52:10 +0000

    day 2 halfway

--- day_2/main.py
+++ day_2/main.py
@@ -2,16 +2,16 @@
 
 #String
 
-"Hello"
-print("Hello"[0])
+# "Hello"
+# print("Hello"[0])
 
-"Hello"[0]
+# "Hello"[0]
 
 #Integer
 
-print(123 + 345)
+# print(123 + 345)
 
-12345678 = 123_456_78
+#12345678 = 123_456_78
 
 
 #float
@@ -20,3 +20,66 @@ print(123 + 345)
 #Boolean
 True
 False
+
+#type()
+#print(type())
+
+# num_char = len(input("What is your name?"))
+
+# new_num_char = str(num_char)
+
+# print("your name has " + new_num_char + " characters")
+
+a = str(123)
+print(type(a))
+
+# print(70 + float("100.5"))
+print(str(70)+ str(100))
+
+
+# two_digit_number = input()
+# # 🚨 Don't change the code above 👆
+# ####################################
+# # Write your code below this line 👇
+# print(int(two_digit_number[0]) + int(two_digit_number[1]))
+
+# two_digit_number = input()
+
+# first_digit = int(two_digit_number[0])
+# second_digit = int(two_digit_number[1])
+
+# Add the two integers together
+# two_digit_number = first_digit + second_digit
+
+# print(two_digit_number)
+
+# 3 + 5
+# 7 - 4
+# 3 * 2
+# 6 / 3
+# 2 ** 2 #the power of exponant
+
+# PEMDASLR  PARENTHESES exponant MULTIPLICATION DIVISION ADDITION SUBTRACTION
+print(3 * 3 + 3 / 3 -3)
+
+# BMI CALCULATOR
+# weight_as_int = int(weight)
+# height_as_float = float(height)
+# # Using the exponent operator **
+# bmi = weight_as_int / height_as_float ** 2
+# # or using multiplication and PEMDAS
+# bmi = weight_as_int / (height_as_float * height_as_float)
+
+# bmi_as_int = int(bmi)
+# print(bmi_as_int)
+
+# print(int(8/3))
+# print(round(8/3))
+# print(round(8/3 ,2))
+
+# score = 0
+# score = score + 1
+
+# use instead
+# score += 1
+# print(score)
