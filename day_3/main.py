926e3ad98a208d788ea0dd16369098510a40c172
DaveTrev <trevaskisdavid@gmail.com>
2024-03-11 08:21:58 +0000

    day three exercises

--- day_3/main.py
+++ day_3/main.py
@@ -1,7 +1,65 @@
-print("Welcome to the rollercoaster!")
-height = int(input("What is your height in cm? "))
+#Price Calc
+# print("Welcome to the rollercoaster!")
+# height = int(input("What is your height in cm? "))
 
-if height > 120:
-    print("You can ride the rollercoaster!")
-else:
-    print("Sorry, you have to grow taller before you can ride.")
+# if height >= 120:
+#     print("You can ride the rollercoaster!")
+#     age = int(input("What is your age?"))
+#     if age < 12:
+#         print("Please pay $5.")
+#     elif age <= 18:
+#         print("Please pay $7.")
+#     else:
+#         print("Please pay $12.")
+# else:
+#     print("Sorry, you have to grow taller before you can ride.")
+
+
+# # Which number do you want to check?
+# number = int(input())
+
+# # If the number can be divided by 2 with 0 remainder.
+# if number % 2 == 0:
+#   print("This is an even number.")
+# # Otherwise (number cannot be divided by 2 with 0 remainder).
+# else:
+#   print("This is an odd number.")
+
+
+##BMI CALC
+# # Enter your height in meters e.g., 1.55
+# height = float(input())
+# # Enter your weight in kilograms e.g., 72
+# weight = int(input())
+# # 🚨 Don't change the code above 👆
+
+# #Write your code below this line 👇
+# bmi = weight / (height * height)
+
+# if bmi < 18.5:
+#   print(f"Your BMI is {bmi}, you are underweight.")
+# elif bmi < 25.0:
+#   print(f"Your BMI is {bmi}, you have a normal weight.")
+# elif bmi < 30:
+#   print(f"Your BMI is {bmi}, you are slightly overweight.")
+# elif bmi < 35:
+#   print(f"Your BMI is {bmi}, you are obese.")
+# else:
+#   print(f"Your BMI is {bmi}, you are clinically obese.")
+
+# leap year check
+# Which year do you want to check?
+# year = int(input())
+# # 🚨 Don't change the code above 👆
+
+# # Write your code below this line 👇
+# if year % 4 == 0:
+#   if year % 100 == 0:
+#     if year % 400 == 0:
+#       print("Leap year")
+#     else:
+#       print("Not leap year")
+#   else:
+#     print("Leap year")
+# else:
+#   print("Not leap year ")
