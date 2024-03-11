0f4777d064d4ed1bdacd24bc6941fc83faa0821a
DaveTrev <trevaskisdavid@gmail.com>
2024-03-11 08:35:48 +0000

    day three exercises

--- day_3/main.py
+++ day_3/main.py
@@ -1,23 +1,32 @@
-#Price Calc
-# print("Welcome to the rollercoaster!")
-# height = int(input("What is your height in cm? "))
 
-# if height >= 120:
-#     print("You can ride the rollercoaster!")
-#     age = int(input("What is your age?"))
-#     if age < 12:
-#         print("Please pay $5.")
-#     elif age <= 18:
-#         print("Please pay $7.")
-#     else:
-#         print("Please pay $12.")
-# else:
-#     print("Sorry, you have to grow taller before you can ride.")
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+bill = 0
+
+if height >= 120:
+    print("You can ride the rollercoaster!")
+    age = int(input("What is your age?"))
+    if age < 12:
+        bill = 5
+        print("Child tickets are $5.")
+    elif age <= 18:
+        bill = 7
+        print("Youth tickets are $7.")
+    else:
+        bill = 12
+        print("adult tickets are $12.")
+
+    wants_photo = input("Do you want a photo taken? Y or N.")
+    if wants_photo == "Y":
+        bill += 3
+    
+    print(f"Your final bill is {bill}")
+else:
+    print("Sorry, you have to grow taller before you can ride.")
 
 
 # # Which number do you want to check?
 # number = int(input())
-
 # # If the number can be divided by 2 with 0 remainder.
 # if number % 2 == 0:
 #   print("This is an even number.")
