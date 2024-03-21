f94226f42b75185597b387686aed143c424c3720
DaveTrev <trevaskisdavid@gmail.com>
2024-03-21 22:31:10 +0000

    day 4 exercises

--- day_4/main.py
+++ day_4/main.py
@@ -17,3 +17,28 @@
 #   print("Heads")
 # else:
 #   print("Tails")
+
+# states_of_america = [
+#     "Delaware", "Pennsylvania", "New Jersey", "Georgia", "Connecticut", 
+#     "Massachusetts", "Maryland", "South Carolina", "New Hampshire", "Virginia", 
+#     "New York", "North Carolina", "Rhode Island", "Vermont", "Kentucky", 
+#     "Tennessee", "Ohio", "Louisiana", "Indiana", "Mississippi", 
+#     "Illinois", "Alabama", "Maine", "Missouri", "Arkansas", 
+#     "Michigan", "Florida", "Texas", "Iowa", "Wisconsin", 
+#     "California", "Minnesota", "Oregon", "Kansas", "West Virginia", 
+#     "Nevada", "Nebraska", "Colorado", "North Dakota", "South Dakota", 
+#     "Montana", "Washington", "Idaho", "Wyoming", "Utah", 
+#     "Oklahoma", "New Mexico", "Arizona", "Alaska", "Hawaii"
+# ]
+
+# print(states_of_america[0])
+
+# names = names_string.split(", ")
+# # names_string contains the input values provided. 
+# # The code above converts the input into an array seperating
+# # each name in the input by a comma and space.
+# # 🚨 Don't change the code above 👆
+# import random
+
+# randomchoice = random.choice(names)
+# print(f"{randomchoice} is going to buy the meal today!")
