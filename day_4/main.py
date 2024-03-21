eae46e4bdfbaae771f4383e3465a739e0c55d084
DaveTrev <trevaskisdavid@gmail.com>
2024-03-21 22:42:05 +0000

    day 4

--- day_4/main.py
+++ day_4/main.py
@@ -42,3 +42,21 @@
 
 # randomchoice = random.choice(names)
 # print(f"{randomchoice} is going to buy the meal today!")
+
+# names = names_string.split(", ")
+
+# import random
+
+# # Get the total number of items in list.
+# num_items = len(names)
+# # Generate random numbers between 0 and the last index. 
+# random_choice = random.randint(0, num_items - 1)
+# # Choose and print a random name.
+# print(names[random_choice])
+
+fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+ 
+dirty_dozen = [fruits, vegetables]
+ 
+print(dirty_dozen[1][1])
